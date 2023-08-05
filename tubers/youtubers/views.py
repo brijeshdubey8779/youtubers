c4ec1a75a33899461d12b8aec5e09f84e2ad5b82
brijeshdubey8779 <brijeshdubey8779@gmail.com>
2023-08-05 22:39:34 +0530

    tubers page added and linked and separate youtubers page is also connected

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Youtubers
 # Create your views here.
 
 
 def youtubers(request):
-    pass
-
+    tubers = Youtubers.objects.order_by('-created_date')
+    data={
+        'tubers':tubers
+    }
+    return render(request, 'youtubers/youtubers.html', data)
 
 def youtubers_detail(request, id):
-    pass
+    tuber = get_object_or_404(Youtubers, pk=id)
+    data={
+        'tuber': tuber
+    }
+    return render(request, 'youtubers/youtubers_detail.html', data)
 
 
 def search(request):
